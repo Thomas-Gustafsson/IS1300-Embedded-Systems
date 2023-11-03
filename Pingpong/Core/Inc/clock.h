8631281b8021f5c6071a9e242c45a6e624342092
Thomas-Gustafsson <90902871+Thomas-Gustafsson@users.noreply.github.com>
2023-11-03 13:04:50 +0100

    first commit

---
+++ Pingpong/Core/Inc/clock.h
@@ -0,0 +1,14 @@
+/*
+ * clock.h
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: thomas
+ */
+#include <stdint.h>
+
+#ifndef INC_CLOCK_H_
+#define INC_CLOCK_H_
+
+void SystemClock_Config(void);
+
+#endif /* INC_CLOCK_H_ */
---
+++ Pingpong/Debug/Core/Src/pingpong.d
@@ -0,0 +1,2 @@
+Core/Src/pingpong.o: ../Core/Src/pingpong.c ../Core/Inc/Pingpong.h
+../Core/Inc/Pingpong.h:
